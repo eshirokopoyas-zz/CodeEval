f6a840d10abaeb47919ff68ddb28f232123c414a
Armadil <eshirokopoyas@iitdgroup.ru>
2015-05-21 22:07:10 +0300

    Отправленный результат по EvenNumbers  .

---
+++ EvenNumbers/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by eshirokopoyas on 21.05.2015.
+ */
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        File file = new File(args[0]);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            System.out.println(~Integer.valueOf(line)&1);
+        }
+    }
+}
---
+++ JSOMMenuIDs/src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by eshirokopoyas on 21.05.2015.
+ */
+public class Main {
+}
