7e23f062dcb20931118974133896b699cbb0c466
Armadil <eshirokopoyas@iitdgroup.ru>
2016-11-03 21:20:35 +0300

    init

--- WorkingExperience/src/Main.java
+++ WorkingExperience/src/Main.java
@@ -3,55 +3,102 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.util.*;
+import java.util.stream.Stream;
 
-/**
+/** TEST
  * Created by eshirokopoyas on 27.05.2015.
  */
 public class Main
 {
-    public static Calendar calendar = Calendar.getInstance();
+    private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendPattern("MMM yyyy").parseDefaulting(ChronoField.DAY_OF_MONTH,1).toFormatter(Locale.ENGLISH);
+
+    private static final String DELIMITER1="-";
+    private static final String DELIMITER2=";";
+
     public static void main(String[] args) throws IOException
     {
-
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM yyyy", Locale.ENGLISH);
 //        File file = new File(args[0]);
+
+
         File file = new File("c:\\1\\1.txt");
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String line;
         while ((line = bufferedReader.readLine()) != null)
         {
-            System.out.println(line);
+            System.out.println(line.trim());
         }
     }
 
-    class DateInterval
+    private static int getMonth(String line)
     {
-        Calendar start;
-        Calendar end;
-        public DateInterval(Calendar a, Calendar b)
+        List<DateInterval> dateIntervalList = new ArrayList<>();
+        for (String string:line.split(DELIMITER2))
         {
-            this.start=a;
-            this.end=b;
-            this.end.add(Calendar.DAY_OF_MONTH,this.end.getActualMaximum(Calendar.DAY_OF_MONTH));
+            dateIntervalList.add(new DateInterval(string));
+        }
+        return 0;
+    }
 
+    private static class DateInterval
+    {
+        private LocalDate start;
+        private LocalDate end;
+
+        private DateInterval(LocalDate start, LocalDate end)
+        {
+            this.start = start;
+            this.end = end;
         }
 
-        public boolean areCrossed(DateInterval dateInterval)
+        private DateInterval(String interval)
         {
-            return !(this.start.after(dateInterval.end)||this.end.before(dateInterval.start));
+            String[] dates = interval.split(DELIMITER1);
+            this.start=parse(dates[0]);
+            this.end=parse(dates[1]).plus(1,ChronoUnit.MONTHS).minus(1,ChronoUnit.DAYS);
         }
 
-        public DateInterval merge(DateInterval dateInterval)
+        @Override
+        public String toString()
         {
-            if (this.start.after(dateInterval.start))
-                return new DateInterval(dateInterval.start,this.end);
-            else
-            {
-                return new DateInterval(this.start,dateInterval.end);
-            }
+            return "start: " + start + ", end: " + end;
         }
+
+        public long getMonths()
+        {
+            return Period.between(start,end.plus(1,ChronoUnit.DAYS)).toTotalMonths();
+        }
+
+        public boolean isNotIntersectedWith(DateInterval dateInterval)
+        {
+            return this.start.isAfter(dateInterval.end)
+                    ||this.end.isBefore(dateInterval.start);
+        }
+
+        public boolean isAdjoinWith(DateInterval dateInterval)
+        {
+            return this.start.isEqual(dateInterval.end)
+                    ||this.end.isEqual(dateInterval.start);
+        }
+
+        public DateInterval mergeIntervals(DateInterval dateInterval1, DateInterval dateInterval2)
+        {
+            return new DateInterval(dateInterval1.start.isBefore(dateInterval2.start)?dateInterval1.start:dateInterval2.start,dateInterval1.end.isAfter(dateInterval2.end)?dateInterval1.end:dateInterval2.end);
+        }
+    }
+
+    private static LocalDate parse(String date)
+    {
+        return LocalDate.parse(date, formatter);
     }
 }
