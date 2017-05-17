134e363f65dbff29e7285fa786efafaf6d265d36
Armadil <eshirokopoyas@iitdgroup.ru>
2017-05-17 19:13:22 +0300

    Finally done

--- WorkingExperience/src/Main.java
+++ WorkingExperience/src/Main.java
@@ -2,19 +2,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.BitSet;
+import java.util.Locale;
 
 /** TEST
  * Created by eshirokopoyas on 27.05.2015.
@@ -24,77 +19,55 @@ public class Main
     private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendPattern("MMM yyyy").parseDefaulting(ChronoField.DAY_OF_MONTH,1).toFormatter(Locale.ENGLISH);
 
     private static final String DELIMITER1="-";
-    private static final String DELIMITER2=";";
+    private static final String DELIMITER2="; ";
+    
+    private static final LocalDate zero = LocalDate.of(1990,1,1);
 
     public static void main(String[] args) throws IOException
     {
-//        File file = new File(args[0]);
+        File file = new File(args[0]);
 
 
-        File file = new File("c:\\1\\1.txt");
+//        File file = new File("c:\\1\\1.txt");
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String line;
         while ((line = bufferedReader.readLine()) != null)
         {
-            System.out.println(line.trim());
+            System.out.println(processLine(line));
         }
-    }
-
-    private static int getMonth(String line)
-    {
-        List<DateInterval> dateIntervalList = new ArrayList<>();
-        for (String string:line.split(DELIMITER2))
-        {
-            dateIntervalList.add(new DateInterval(string));
-        }
-        return 0;
     }
 
     private static class DateInterval
     {
-        private LocalDate start;
-        private LocalDate end;
-
-        private DateInterval(LocalDate start, LocalDate end)
-        {
-            this.start = start;
-            this.end = end;
-        }
-
-        private DateInterval(String interval)
-        {
-            String[] dates = interval.split(DELIMITER1);
-            this.start=parse(dates[0]);
-            this.end=parse(dates[1]).plus(1,ChronoUnit.MONTHS).minus(1,ChronoUnit.DAYS);
-        }
-
-        @Override
-        public String toString()
-        {
-            return "start: " + start + ", end: " + end;
-        }
-
-        public long getMonths()
-        {
-            return Period.between(start,end.plus(1,ChronoUnit.DAYS)).toTotalMonths();
-        }
-
-        public boolean isNotIntersectedWith(DateInterval dateInterval)
-        {
-            return this.start.isAfter(dateInterval.end)
-                    ||this.end.isBefore(dateInterval.start);
-        }
-
-        public boolean isAdjoinWith(DateInterval dateInterval)
-        {
-            return this.start.isEqual(dateInterval.end)
-                    ||this.end.isEqual(dateInterval.start);
-        }
-
-        public DateInterval mergeIntervals(DateInterval dateInterval1, DateInterval dateInterval2)
-        {
-            return new DateInterval(dateInterval1.start.isBefore(dateInterval2.start)?dateInterval1.start:dateInterval2.start,dateInterval1.end.isAfter(dateInterval2.end)?dateInterval1.end:dateInterval2.end);
-        }
+	    private LocalDate start;
+	    private LocalDate end;
+	    
+	    private DateInterval(String interval)
+	    {
+		    String[] dates = interval.split(DELIMITER1);
+		    this.start = parse(dates[0]);
+		    this.end = parse(dates[1]).plus(1,ChronoUnit.DAYS);
+	    }
+	
+	    @Override
+	    public String toString()
+	    {
+		    return "start: " + start + ", end: " + end;
+	    }
+    }
+    
+    private static int processLine(String line)
+    {
+    	BitSet bitSet = new BitSet(360);
+    	
+    	for (String intervalString:line.split(DELIMITER2))
+	    {
+		    DateInterval dateInterval = new DateInterval(intervalString);
+		    int startMonth = (int) Period.between(zero,dateInterval.start).toTotalMonths();
+		    int endMonth = (int) Period.between(zero,dateInterval.end).toTotalMonths();
+		    bitSet.set(startMonth,endMonth+1);
+	    }
+    	return bitSet.cardinality()/12;
     }
 
     private static LocalDate parse(String date)
